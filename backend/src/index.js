eb3f96ed2f1627ee18099dbe3420480241f3443a
jaakko <jaakko.j.virtanen@tuni.fi>
2020-12-28 15:07:28 -0500

    Backend now working
    Closes #4

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,34 @@
-const app = require('./app');
+import Express from "express";
+import faker from "faker";
 
-const port = process.env.PORT || 5000;
+
+const app = Express(); 
+const port = 5050;
+
+
+
+
+ app.get("/task", (req,res) => {
+
+    res.json({name :faker.name.firstName(), created :Date(), completed :null})
+    
+
+ })
+
+ 
+
+app.listen(port, () => console.log("listening on port" + port))
+
+
+
+
+
+
+
+/*
+const port = process.env.PORT || 5050;
 app.listen(port, () => {
   /* eslint-disable no-console */
-  console.log(`Listening: http://localhost:${port}`);
+  //console.log(`Listening: http://localhost:${port}`);
   /* eslint-enable no-console */
-});
+//});
